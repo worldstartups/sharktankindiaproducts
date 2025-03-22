8ff776124b5906948ac1eb75b1335d6ac7f36759
worldstartups <ashlesha4475@gmail.com>
2025-03-22 17:59:37 +0530

    push

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/sharktankglobal">
+  <BrowserRouter basename="/sharktankindiaproducts">
     <App />
   </BrowserRouter>
 );
