2dc2619493cabb9cf3a9bf5cf9fbaff616bab0a2
worldstartups <ashlesha4475@gmail.com>
2025-03-22 18:52:31 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";  // Import the HomePage component
-import Header from "./components/Header"; // Import the Header component
 
 function App() {
   return (
     <Router>
-      {/* Header will be displayed on all pages */}
-      <Header />
       <Routes>
         {/* Route to HomePage */}
-        <Route path="/" element={<HomePage />} />  
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </Router>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-// HomePage.js will show just the content for the homepage
 function HomePage() {
   return (
     <div>
       <h1>Welcome to Shark Tank India</h1>
-      <p>Here you can explore various seasons, investors, and products.</p>
     </div>
   );
 }
